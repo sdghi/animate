45aa6b23c68e0293aa5b82507f9181e5b7d55f09
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-06-23 19:18:37 -1000

    edit: changes image gallery to use flexbox

--- demo/scripts/main.js
+++ demo/scripts/main.js
@@ -3,7 +3,6 @@ import { Watcher, screen, flip, crossfade } from '../../lib';
 const textBlockSection = new Watcher('.text-block');
 
 // Have access the event of window.addEventListener
-screen('<=768', (e) => console.log('above tablet', e));
 screen('>768', () => console.log('below tablet'));
 
 textBlockSection.scroll(
@@ -27,32 +26,35 @@ textBlockSection.scroll(
 	{ rootMargin: '-50%' }
 );
 
-const galleryImages = document.querySelectorAll('.gallery-image');
-const featuredImage = document.querySelector('.featured-image');
+screen('<=768', (e) => {
+	const galleryImages = document.querySelectorAll('.image-gallery__image');
+	const featuredElement = document.querySelector('.image-gallery__featured');
 
-galleryImages.forEach((image, i) => {
-	const currentSrc = image.getAttribute('src');
-	image.addEventListener('click', () => {
+	galleryImages.forEach((image, i) => {
+		const currentSrc = image.getAttribute('src');
+		image.addEventListener('click', () => {
+			crossfade(
+				`.image-gallery__image[data-key="${i + 1}"]`,
+				'.image-gallery__featured',
+				(el) => {
+					el.style.visibility = 'visible';
+					el.setAttribute('data-current', i + 1);
+					el.setAttribute('src', currentSrc);
+				}
+			);
+		});
+	});
+
+	featuredElement.addEventListener('click', () => {
+		const currentIndex = featuredElement.getAttribute('data-current');
+
 		crossfade(
-			`.gallery-image[data-key="${i + 1}"]`,
-			'.featured-image',
+			'.image-gallery__featured',
+			`.image-gallery__image[data-key="${currentIndex}"]`,
 			(el) => {
 				el.style.visibility = 'visible';
-				el.setAttribute('data-current', i + 1);
-				el.setAttribute('src', currentSrc);
-			}
+			},
+			{ duration: 400 }
 		);
 	});
-});
-
-featuredImage.addEventListener('click', () => {
-	const currentIndex = featuredImage.getAttribute('data-current');
-
-	crossfade(
-		'.featured-image',
-		`.gallery-image[data-key="${currentIndex}"]`,
-		(el) => {
-			el.style.visibility = 'visible';
-		}
-	);
 });
