5c2713cc5c1c6d0c20942d2005930732b3a7a548
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-07-05 23:22:24 -1000

    edit: styles the image gallery

--- demo/scripts/main.js
+++ demo/scripts/main.js
@@ -25,6 +25,9 @@ textBlockSection.scroll(
 
 const galleryImages = document.querySelectorAll('.image-gallery__image');
 const featuredElement = document.querySelector('.image-gallery__featured');
+const featuredContainer = document.querySelector(
+	'.image-gallery__featured-container'
+);
 
 galleryImages.forEach((image, i) => {
 	const currentSrc = image.getAttribute('src');
@@ -33,6 +36,7 @@ galleryImages.forEach((image, i) => {
 			`.image-gallery__image[data-key="${i + 1}"]`,
 			'.image-gallery__featured',
 			(el) => {
+				featuredContainer.classList.add('visible');
 				console.log('running crossfade');
 				el.style.visibility = 'visible';
 				el.setAttribute('data-current', i + 1);
@@ -42,13 +46,14 @@ galleryImages.forEach((image, i) => {
 	});
 });
 
-featuredElement.addEventListener('click', () => {
+featuredContainer.addEventListener('click', () => {
 	const currentIndex = featuredElement.getAttribute('data-current');
 
 	crossfade(
 		'.image-gallery__featured',
 		`.image-gallery__image[data-key="${currentIndex}"]`,
 		(el) => {
+			featuredContainer.classList.remove('visible');
 			el.style.visibility = 'visible';
 		},
 		{ duration: 400 }
