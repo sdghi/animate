76bc9184a0c344f8d90ad6d52b63f48e110e4eef
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-07-17 22:48:57 -1000

    feat: adds jsdoc for getElement and getAllElements

--- lib/helpers/getElement.ts
+++ lib/helpers/getElement.ts
@@ -1,25 +1,33 @@
 import { Selector } from '../types';
 
 /**
- *
+ * Return the DOM element using either a string or selector
  * @param el A string or DOM node
+ * @param parent Parent to run querySelector defaults to document
+ * @returns DOM Element
  */
 export function getElement(
-	el: Selector,
-	parent: HTMLElement | Document = document
+  el: Selector,
+  parent: HTMLElement | Document = document
 ) {
-	if (typeof el === 'string') {
-		return parent.querySelector(el);
-	}
-	return el;
+  if (typeof el === 'string') {
+    return parent.querySelector(el);
+  }
+  return el;
 }
 
+/**
+ * Return a NodeList using either a string or selector
+ * @param el A string or DOM node
+ * @param parent Parent to run querySelectorAll defaults to document
+ * @returns NodeList
+ */
 export function getAllElements(
-	el: string,
-	parent: HTMLElement | Document = document
+  el: string,
+  parent: HTMLElement | Document = document
 ) {
-	if (typeof el === 'string') {
-		return parent.querySelectorAll(el);
-	}
-	return el;
+  if (typeof el === 'string') {
+    return parent.querySelectorAll(el);
+  }
+  return el;
 }
