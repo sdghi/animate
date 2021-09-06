5761c9e3b13b1c64a0ea7e10a207bf256b0bae82
frzrbox <davidefhan@hey.com>
2021-09-05 21:52:09 -1000

    Updates package.json

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
 // vite.config.js
 const path = require('path');
+import { defineConfig } from 'vite';
 import liveReload from 'vite-plugin-live-reload';
 
 /**
  * @type {import('vite').UserConfig}
  */
 
-module.exports = {
-  plugins: [liveReload('/demo/scss/**/*.scss')],
+module.exports = defineConfig({
   build: {
+    target: 'esnext',
     lib: {
-      entry: path.resolve(__dirname, 'lib/index.ts'),
+      entry: path.resolve(__dirname, '/lib/index.ts'),
       name: 'humdinger',
+      formats: ['es', 'umd', 'iife'],
     },
-    rollupOptions: {},
   },
-};
+  plugins: [liveReload('/demo/scss/**/*.scss')],
+});
