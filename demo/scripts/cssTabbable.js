2afc2c846eeb712cd5538610a1cac62fb6c9fa37
frzrbox <davidefhan@hey.com>
2021-09-06 00:06:48 -1000

    Removes demo dir

--- demo/scripts/cssTabbable.js
+++
@@ -1,9 +0,0 @@
-import { watchHeightChange } from '../../lib';
-
-const tabElement = document.querySelector('.css-tabbable__tab');
-
-watchHeightChange(
-  tabElement,
-  ({ afterHeight }) => console.log(`Height has changed to ${afterHeight}`),
-  { duration: 200 }
-).observe();
