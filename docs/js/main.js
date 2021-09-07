b7f61d4937c7c1e485981bf006995ea4e954defe
frzrbox <davidefhan@hey.com>
2021-09-06 15:53:32 -1000

    Adds base layout of home

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,31 @@
-console.log('connected', humdinger);
+const { Watcher, watchHeightChange } = humdinger;
+
+const homeScrollWatcher = new Watcher('.home__scroll__illustration');
+
+homeScrollWatcher.scroll(
+  'enter',
+  (el) => {
+    console.log('enter');
+    el.setAttribute('data-in-view', true);
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+homeScrollWatcher.scroll(
+  'exit',
+  (el) => {
+    console.log('exit');
+    el.removeAttribute('data-in-view');
+  },
+  {
+    threshold: 0,
+  }
+);
+
+const detailElements = document.querySelectorAll('details');
+
+detailElements.forEach((el) => {
+  watchHeightChange(el).observe();
+});
