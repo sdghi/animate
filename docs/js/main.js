c36fff12cb2699eea2020d2affa4f9366a517c40
frzrbox <davidefhan@hey.com>
2021-09-06 17:16:06 -1000

    Styles homepage

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,11 +1,25 @@
 const { Watcher, watchHeightChange } = humdinger;
 
 const homeScrollWatcher = new Watcher('.home__scroll__illustration');
+const scrollElements = document.querySelectorAll('[data-scroll]');
+
+scrollElements.forEach((el) => {
+  const element = new Watcher(el);
+
+  element.scroll(
+    'enter',
+    () => {
+      el.setAttribute('data-in-view', true);
+    },
+    {
+      threshold: 1,
+    }
+  );
+});
 
 homeScrollWatcher.scroll(
   'enter',
   (el) => {
-    console.log('enter');
     el.setAttribute('data-in-view', true);
   },
   {
@@ -16,7 +30,6 @@ homeScrollWatcher.scroll(
 homeScrollWatcher.scroll(
   'exit',
   (el) => {
-    console.log('exit');
     el.removeAttribute('data-in-view');
   },
   {
