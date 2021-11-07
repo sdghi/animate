e85e483cb51b73c2f9c567b527aeba4802a9a9aa
frzrbox <davidefhan@hey.com>
2021-11-06 22:22:02 -1000

    Changes type of timerFn

--- custom.d.ts
+++ custom.d.ts
@@ -1,22 +1,22 @@
 interface FLIPOptions {
-  duration?: number;
-  easing?: string;
-  direction?: string;
-  scale?: boolean;
-  done?: CallableFunction;
+	duration?: number;
+	easing?: string;
+	direction?: string;
+	scale?: boolean;
+	done?: CallableFunction;
 }
 
 type Selector = string | HTMLElement | Element;
 
 interface WAAPIOptions {
-  duration: number;
-  easing: string;
-  fill?: 'forwards';
+	duration: number;
+	easing: string;
+	fill?: "forwards";
 }
 
 interface GalleryOptions {
-  itemSelector: string;
-  start: number;
-  timer?: number;
-  timerFn?: CallableFunction;
+	itemSelector: string;
+	start: number;
+	timer?: number;
+	timerFn?: any;
 }
